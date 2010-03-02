a8a00abedab639e1b66a8cb9ae64d179f4c3794b
Anant Shrivastava <anant@anantshri.info>
2010-03-02 20:08:51 +0000

    updated and added some more parsing data

--- test.php
+++ test.php
@@ -3,7 +3,11 @@ include('readme_parser_class.php');
 $wp_profiles = new wp_readme_parser($_GET['plugin']);
 //echo $wp_profiles->getPlug_name();
 //echo "^" . $wp_profiles->getStable() . "^";
-//echo $wp_profiles->getUrl();
-echo $wp_profiles->getTags();
+echo $wp_profiles->getUrl();
+echo "<br />" . $wp_profiles->getStable();
+echo "<br />" . $wp_profiles->getTags();
+echo "<br />" . $wp_profiles->getRequire();
+echo "<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />";
+echo "<br />" . $wp_profiles->getData();
 echo "\n<br />Mem : " . memory_get_usage();
 ?>
